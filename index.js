8b06798ebc5345d1df252f8840a1aa4c176fa0c1
+ <+>
2024-01-13 22:23:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp){
+    const blocks=(((pickUp-42)**2)**0.5);
+    return blocks;
+}
+
+
+function distanceFromHqInFeet(pickUp){
+    const blocks = (((pickUp-42)**2)**0.5);
+    const feet=blocks*264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start,destination){
+    const blocks=(((start-destination)**2)**0.5);
+    const feet=blocks*264
+    return feet;
+  
+    
+
+}
+
+function calculatesFarePrice(start,destination){
+    const blocks=(((start - destination)**2)**0.5);
+    const feet = blocks *264;
+    let fare;
+    if (feet<= 400) {
+        return 0;
+}else if (feet>400 && feet <=2000) {
+    fare = (2 *(feet-400))/100;
+    return fare;
+}else if(feet > 2000 && feet <= 2500){
+ return 25;   
+}else{
+ return 'cannot travel that far';
+}
+}
